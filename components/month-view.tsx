9ac3866128860926057b059607060b3d92b8f11d
Andrey Muravin <muravin@bk.ru>
2024-11-08 22:03:15 +0300

    Отображение календаря недели

--- components/month-view.tsx
+++ components/month-view.tsx
@@ -8,7 +8,7 @@ export default function MonthView() {
 
     const { twoDMonthArray } = useDateStore();
     return (
-        <section className="grid grid-cols-7 grid-rows-5 lg:h-[100vh]">
+        <section className="grid grid-cols-7 grid-rows-5 lg:h-[100vh] h-[60vh]">
             {
                 twoDMonthArray.map((row, i) => (
                     <Fragment key={i}>
--- lib/getTime.ts
+++ lib/getTime.ts
@@ -1,5 +1,12 @@
 
 import dayjs from "dayjs";
+import "dayjs/locale/ru"; // Импортируем русский локаль  
+
+dayjs.locale('ru'); // Устанавливаем русский язык  
+
+export const isCurrentDay = (day: dayjs.Dayjs) => {
+    return day.isSame(dayjs(), "day")
+};
 
 export const getMonth = (month = dayjs().month()) => {
     dayjs.locale('ru');  
@@ -16,3 +23,27 @@ export const getMonth = (month = dayjs().month()) => {
         Array.from({length: 7}, () => dayjs(new Date(year, month, ++dayCounter)))
     );
 };   
+
+export const getWeekDays = (date: dayjs.Dayjs) => {
+
+
+    const startOfWeek = date.startOf("week"); 
+    const weekDates = [];
+
+    for (let i = 0; i < 7; i++) {
+
+        const currentDate = startOfWeek.add(i, "day");
+        weekDates.push({
+            currentDate,
+            today:
+                currentDate.toDate().toDateString() === dayjs().toDate().toDateString(),
+            isCurrentDay,
+        });
+    };
+
+    return weekDates;
+};
+
+export const getHours = Array.from({ length: 24 }, (_, i) =>
+    dayjs().startOf("day").add(i, "hour"),
+  );
